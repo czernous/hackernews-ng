e780950c0edcfb75ffbf7b554cee9bd48c0eb41c
Anton Chernous <a.chernous@notamedia.ru>
2022-01-28 13:22:19 +0300

    update randomizing algorithm in getRandomItems func

--- src/app/helpers/data-helpers.ts
+++ src/app/helpers/data-helpers.ts
@@ -1,2 +1,7 @@
-export const getRandomItems = (arr: any[], itemLimit: number) =>
-  arr.sort(() => Math.random() - 0.5).slice(0, itemLimit);
+export const getRandomItems = (arr: any[], itemLimit: number) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr.slice(0, itemLimit);
+};
